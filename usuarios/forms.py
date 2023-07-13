b6f630c96a4b2eb5f247b17333bf69afac2ace44
JL <joaolca.2008@gmail.com>
2023-07-13 14:28:58 -0300

    bug fix de estilo e mensagens em lugar errado

---
+++ usuarios/forms.py
@@ -0,0 +1,97 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de login',
+        required=True,
+        max_length=100,
+        widget = forms.TextInput(
+            attrs= {
+                'class': 'form-control',
+                'placeholder': 'Ex: João Silva'
+            }
+        )
+    )
+    senha_login = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget = forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label='Nome de Cadastro',
+        max_length=100,
+        required=True,
+        widget = forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: João Silva'
+            }
+        )
+    )
+    
+    email = forms.CharField(
+        label = 'Email de cadastro',
+        max_length = 100,
+        required = True,
+        widget = forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: eusoudemais@eusoudemais.com'
+            }
+        )
+    )
+    
+    senha_1 = forms.CharField(
+        label = 'Senha',
+        max_length = 70,
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha aqui:'
+            }
+        )
+    )
+    
+    senha_2 = forms.CharField(
+        label = 'Confirme sua senha',
+        max_length = 70,
+        required = True,
+        widget = forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirme sua senha aqui:'
+            }
+        )
+    )
+    
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+        
+        if nome:
+            nome = nome.strip()
+            if ' ' in nome:
+                raise forms.ValidationError('Não é possível inserir espaços dentro do campo usuário!')
+            elif User.objects.filter(username=nome).exists():
+                raise forms.ValidationError('Usuário já existe!')
+            else:
+                return nome
+    
+    def clean_senha_2(self):
+        if self.cleaned_data.get('senha_2') != self.cleaned_data.get('senha_2'):
+            raise forms.ValidationError('As senhas não são iguais!')
+        else:
+            return self.cleaned_data.get('senha_1')
+    
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data.get('email')).exists():
+            raise forms.ValidationError('O email já existe')
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,65 @@
-from django.shortcuts import render
-from usuarios.models import *
+from django.shortcuts import render, redirect
+from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
+from django.contrib import auth, messages
 
 def login(request):
-    return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    
+    if request.POST.get('next', None) not in ('cadastro', 'login', 'logout', '', '/') and not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+    
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+        
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha_login'].value()
+            
+            usuario = auth.authenticate(
+                request,
+                username=nome,
+                password=senha
+            )
+            
+            if usuario is not None:
+                auth.login(request, usuario)
+                messages.success(request, f'{usuario.get_username()} logado com sucesso')
+                return redirect('index')
+            else:
+                messages.error(request, 'Erro ao efetuar login!')
+                return redirect('login')
+    
+    return render(request, 'usuarios/login.html', {'form': form})
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+    
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+        
+        if form.is_valid():
+            nome = form['nome_cadastro'].value()
+            email = form['email'].value()
+            senha = form['senha_1'].value()
+            
+            
+            usuario = User.objects.create_user(
+                username = nome,
+                email = email,
+                password = senha,
+            )
+            
+            usuario.save()
+            messages.success(request, 'Cadastro efetuado com sucesso!')
+            return redirect('login')
+            
+    return render(request, 'usuarios/cadastro.html', {'form': form})
+
+def logout(request):
+    user = auth.get_user(request)
+    user = '' if str(user) == 'AnonymousUser' else user
+    messages.success(request, f'{user} Deslogado com sucesso!')
+    auth.logout(request)
+    return redirect('login')
